d4c89e661e86c141fa92f04ba3d26ad475185d6c
Gautham <gthampies@gmail.com>
2025-06-13 17:42:11 +0530

    Fix:Updated version

--- assessments/week09/src/main.cpp
+++ assessments/week09/src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <thread>
-#include "Recorder.h"  // Make sure path/case matches your file
-
+#include "Recorder.h"  
 using namespace std;
 
 void loadUserData(Recorder& rh, const string& filename) {
@@ -15,25 +14,17 @@ void loadOperatorData(Recorder& rh, const string& filename) {
 int main() {
     Recorder rh;
     string filename = "data.txt";
-
-    // Start two threads for user and operator data loading
     thread t1(loadUserData, ref(rh), filename);
     thread t2(loadOperatorData, ref(rh), filename);
-
-    // Wait for both threads to complete
     t1.join();
     t2.join();
-
     int choice;
     do {
-        cout << "\n===== CDR Billing System Menu =====\n";
-        cout << "1. Display All Customer Bills (Console)\n";
-        cout << "2. Display All Operator Bills (Console)\n";
+        cout << "1. Display All Customer Bills\n";
+        cout << "2. Display All Operator Bills\n";
         cout << "3. Save Customer Bills to File\n";
         cout << "4. Save Operator Bills to File\n";
-        cout << "5. Display Specific Customer Bill\n";
-        cout << "6. Display Specific Operator Bill\n";
-        cout << "0. Exit\n";
+        cout << "5. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
 
@@ -50,27 +41,13 @@ int main() {
             case 4:
                 rh.displayAllOperatorBillToFile("operator_bill.txt");
                 break;
-            case 5: {
-                int id;
-                cout << "Enter customer ID: ";
-                cin >> id;
-                rh.displayCustomerBill(id);
-                break;
-            }
-            case 6: {
-                int id;
-                cout << "Enter operator ID: ";
-                cin >> id;
-                rh.displayOperatorBill(id);
-                break;
-            }
-            case 0:
-                cout << "Exiting...\n";
+            case 5:
+                cout << "Exiting\n";
                 break;
             default:
-                cout << "Invalid choice! Try again.\n";
+                cout << "Invalid option.\n";
         }
-    } while (choice != 0);
+    } while (choice != 5);
 
     return 0;
 }
